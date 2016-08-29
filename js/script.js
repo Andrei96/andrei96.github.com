6b39c9e7b4c8e2fab888f1b2eea2fc1e5ef39d32
Andrei96 <andrei.nike2012@gmail.com>
2016-08-29 19:56:54 +0300

    azaza

--- js/script.js
+++ js/script.js
@@ -1,37 +1,49 @@
-
 $(document).ready(function() {
-    var arrayImg = ["images/slider1.jpg", "images/nature-full-hd.jpg", "images/4235765-wallpaper-full-hd.jpg", "images/d87623f111226679f540d19309d6c711.jpg", "images/widescreen-full-hd.jpg"];
-    //var arrayImg = ["images/slider1.jpg", "images/widescreen-full-hd.jpg"];
-    var imgSlider = $(".slider_photo");
-    var key = 0;
-    var keyPrevious = 0;
 
-    $(".change-photo-left").on("click", function() {
-        key--;
-        imgSlider.attr("src", nextAndBack(key)).css({"display" : "block","height" : "490px"});
-    });
+    var Slider = function(container) {
+        var $container = $(container);
 
-    $(".change-photo-right").on("click", function() {
-        key++;
-        imgSlider.attr("src", nextAndBack(key)).css({"display" : "block","height" : "490px"});
+        var sliderHandler = function(key) {
+            if (key) {
+                if(count < arraySize) {
+                    return arrayImg[count];
+                }
 
-    });
+                else if(count == arraySize) {
+                    count = 0;
+                    return sliderHandler(true);
+                }
+            } else {
+                if(count == 0) {
+                    count = arraySize;
+                    return sliderHandler(false);
+                } else {
+                    return arrayImg[--count];
+                }
+            }
+        };
 
-    function nextAndBack(keyCount) {
-        keyPrevious++;
-        console.log("KeyPrevious = " + keyPrevious);
-        console.log("KeyCount = " + keyCount);
-        if((keyCount >= arrayImg.length) && (keyPrevious >= arrayImg.length)) {
-            key = 0;
-            keyPrevious = 0;
-            return nextAndBack(keyCount);
-        } else if(keyPrevious > keyCount) {
-            return arrayImg[keyCount--];
-        } else if(keyPrevious == keyCount) {
-            return arrayImg[keyCount];
-        } else if((keyCount <= arrayImg.length) && (keyPrevious <= arrayImg.length)) {
-            console.log("ok");
-        }
-    }
+        var
+            imgSlider = $container.find(".slider_photo"),
+            startImg = imgSlider.attr("src"),
+            arrayImg = [startImg, "images/nature-full-hd.jpg", "images/4235765-wallpaper-full-hd.jpg", "images/d87623f111226679f540d19309d6c711.jpg", "images/widescreen-full-hd.jpg"],
+            arraySize = arrayImg.length,
+            keyLeft = false,
+            keyRight = true,
+            count = 0;
 
+        $container.find(".change-photo-left").click(function() {
+            //imgSlider.attr("src", slider(keyLeft));
+            imgSlider.animate({right: '100vw'}, 'slow', function() {$(this).animate({right: 0})}).attr("src", sliderHandler(keyLeft));
+        });
+
+        $container.find(".change-photo-right").click(function() {
+            count++;
+            imgSlider.animate({left: '100vw'}, 'slow', function() {$(this).animate({left: 0})}).attr("src", sliderHandler(keyRight));
+        });
+    };
+
+    $('.slider').each(function(index, element) {
+        new Slider(element);
+    })
 });
